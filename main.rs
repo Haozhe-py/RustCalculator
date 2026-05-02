287bced3740a225c4ea933ae232394425397c5be
Haozhe Xu <xuhaozhepy@outlook.com>
2026-05-02 13:39:54 +0800

    Enhance welcome message with operator instructions
    Added instructions for supported operators and syntax errors.

--- main.rs
+++ main.rs
@@ -8,6 +8,7 @@ mod calculator;
 fn main()
 {
 	println!("Welcome to use RustCalculator. \nEnter \"quit\" to exit. ");
+	println!("\nThe following operators are supported:\n+ - * / % ^ ( )\n\nPlease DON'T enter any expressions with syntax error, or this program may panic. DON'T use \"=\" at the end of the expression.\n");
 	loop {
 		let mut input=String::new();
 		print!("\n> ");
